2cf3a0505a8f96e0071338625548cb758b59af6d
Dmitry <dmitry.s@code-care.pro>
2022-03-08 22:56:46 +0200

    update drawer functions

---
+++ client/src/pages/Article/drawerFunctions.ts
@@ -0,0 +1,42 @@
+import {Tree, TreeNodeInfo} from '@blueprintjs/core';
+
+export type NodePath = number[];
+
+type TreeAction =
+  | { type: "SET_IS_EXPANDED"; payload: { path: NodePath; isExpanded: boolean } }
+  | { type: "DESELECT_ALL" }
+  | { type: "SET_IS_SELECTED"; payload: { path: NodePath; isSelected: boolean } };
+
+function forEachNode(nodes: TreeNodeInfo[] | undefined, callback: (node: TreeNodeInfo) => void) {
+  if (nodes === undefined) {
+    return;
+  }
+
+  for (const node of nodes) {
+    callback(node);
+    forEachNode(node.childNodes, callback);
+  }
+}
+
+function forNodeAtPath(nodes: TreeNodeInfo[], path: NodePath, callback: (node: TreeNodeInfo) => void) {
+  callback(Tree.nodeFromPath(path, nodes));
+}
+
+export function treeReducer(state: TreeNodeInfo[], action: TreeAction) {
+  switch (action.type) {
+    case "DESELECT_ALL":
+      const newState1 = [...state];
+      forEachNode(newState1, node => (node.isSelected = false));
+      return newState1;
+    case "SET_IS_EXPANDED":
+      const newState2 = [...state];
+      forNodeAtPath(newState2, action.payload.path, node => (node.isExpanded = action.payload.isExpanded));
+      return newState2;
+    case "SET_IS_SELECTED":
+      const newState3 = [...state];
+      forNodeAtPath(newState3, action.payload.path, node => (node.isSelected = action.payload.isSelected));
+      return newState3;
+    default:
+      return state;
+  }
+}
--- client/src/pages/Article/index.tsx
+++ client/src/pages/Article/index.tsx
@@ -1,4 +1,5 @@
 import React, { Fragment, useCallback, useReducer, useState } from 'react';
+import { Link } from 'react-router-dom';
 import {
   Drawer,
   H3,
@@ -11,10 +12,16 @@ import {
   TreeNodeInfo,
 } from '@blueprintjs/core';
 import mainPage from './mainPageData';
+import {treeReducer, NodePath} from './drawerFunctions';
 
 import './Article.css';
 
 const INITIAL_STATE: TreeNodeInfo[] = [
+  {
+    id: 6,
+    label: (<Link className='drawer__link' to='/'>Главная</Link>),
+    isSelected: true,
+  },
   {
     id: 0,
     hasCaret: true,
@@ -22,11 +29,11 @@ const INITIAL_STATE: TreeNodeInfo[] = [
     childNodes: [
       {
         id: 1,
-        label: '123123123',
+        label: (<Link className='drawer__link' to='/article/123'>Главная</Link>),
       },
       {
         id: 2,
-        label: '123123123',
+        label: (<Link className='drawer__link' to='/article/123'>Главная</Link>),
       },
     ],
   },
@@ -57,64 +64,9 @@ const drawerProps = {
   usePortal: true,
 };
 
-// const searchNodeAndReplace = (
-//   nodes: TreeNodeInfo[],
-//   node: TreeNodeInfo
-// ): TreeNodeInfo[] => {
-//   const id = node.id;
-//   const nodeToReplaceIndex = nodes.findIndex((e) => e.id === id);
-
-//   if (nodeToReplaceIndex !== -1) {
-//     nodes[nodeToReplaceIndex] = node;
-//   }
-
-//   return nodes;
-// };
-
-type NodePath = number[];
-
-type TreeAction =
-  | { type: "SET_IS_EXPANDED"; payload: { path: NodePath; isExpanded: boolean } }
-  | { type: "DESELECT_ALL" }
-  | { type: "SET_IS_SELECTED"; payload: { path: NodePath; isSelected: boolean } };
-
-function forEachNode(nodes: TreeNodeInfo[] | undefined, callback: (node: TreeNodeInfo) => void) {
-  if (nodes === undefined) {
-    return;
-  }
-
-  for (const node of nodes) {
-    callback(node);
-    forEachNode(node.childNodes, callback);
-  }
-}
-
-function forNodeAtPath(nodes: TreeNodeInfo[], path: NodePath, callback: (node: TreeNodeInfo) => void) {
-  callback(Tree.nodeFromPath(path, nodes));
-}
-
-function treeExampleReducer(state: TreeNodeInfo[], action: TreeAction) {
-  switch (action.type) {
-    case "DESELECT_ALL":
-      const newState1 = [...state];
-      forEachNode(newState1, node => (node.isSelected = false));
-      return newState1;
-    case "SET_IS_EXPANDED":
-      const newState2 = [...state];
-      forNodeAtPath(newState2, action.payload.path, node => (node.isExpanded = action.payload.isExpanded));
-      return newState2;
-    case "SET_IS_SELECTED":
-      const newState3 = [...state];
-      forNodeAtPath(newState3, action.payload.path, node => (node.isSelected = action.payload.isSelected));
-      return newState3;
-    default:
-      return state;
-  }
-}
-
 export const Article: React.FC = () => {
   const [drawerOpen, setDrawerOpen] = useState(false);
-  const [articles, dispatch] = useReducer(treeExampleReducer, INITIAL_STATE);
+  const [articles, dispatch] = useReducer(treeReducer, INITIAL_STATE);
 
   const handleDrawerToggle = () => setDrawerOpen(!drawerOpen);
 
@@ -124,6 +76,23 @@ export const Article: React.FC = () => {
         payload: { path: nodePath, isExpanded: !_node.isExpanded },
         type: "SET_IS_EXPANDED",
       });
+    },
+    []
+  );
+
+  const handleNodeClick = useCallback(
+    (_node: TreeNodeInfo, nodePath: NodePath) => {
+      if (_node.hasCaret || _node.isSelected) {
+        return
+      }
+
+      dispatch({type: "DESELECT_ALL"});
+      dispatch({
+        payload: { path: nodePath, isSelected: !_node.isSelected },
+        type: "SET_IS_SELECTED",
+      });
+
+      setDrawerOpen(false)
     },
     []
   );
@@ -148,7 +117,7 @@ export const Article: React.FC = () => {
         <div className="drawer__content">
           <Tree
             contents={articles}
-            // onNodeClick={handleNodeClick}
+            onNodeClick={handleNodeClick}
             onNodeCollapse={handleNodeToggle}
             onNodeExpand={handleNodeToggle}
             className="drawer__tree"
