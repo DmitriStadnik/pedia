6b9199ddc5e33e58af133dcf11e5a9d05191e5f8
DmitriStadnik <dmitri.stadnik@gmail.com>
2022-04-30 01:35:54 +0300

    Update linter settings

--- client/src/pages/Article/drawerFunctions.ts
+++ client/src/pages/Article/drawerFunctions.ts
@@ -1,13 +1,22 @@
-import {Tree, TreeNodeInfo} from '@blueprintjs/core';
+import { Tree, TreeNodeInfo } from '@blueprintjs/core';
 
 export type NodePath = number[];
 
 type TreeAction =
-  | { type: "SET_IS_EXPANDED"; payload: { path: NodePath; isExpanded: boolean } }
-  | { type: "DESELECT_ALL" }
-  | { type: "SET_IS_SELECTED"; payload: { path: NodePath; isSelected: boolean } };
+  | {
+      type: 'SET_IS_EXPANDED';
+      payload: { path: NodePath; isExpanded: boolean };
+    }
+  | { type: 'DESELECT_ALL' }
+  | {
+      type: 'SET_IS_SELECTED';
+      payload: { path: NodePath; isSelected: boolean };
+    };
 
-function forEachNode(nodes: TreeNodeInfo[] | undefined, callback: (node: TreeNodeInfo) => void) {
+function forEachNode(
+  nodes: TreeNodeInfo[] | undefined,
+  callback: (node: TreeNodeInfo) => void
+) {
   if (nodes === undefined) {
     return;
   }
@@ -18,23 +27,35 @@ function forEachNode(nodes: TreeNodeInfo[] | undefined, callback: (node: TreeNod
   }
 }
 
-function forNodeAtPath(nodes: TreeNodeInfo[], path: NodePath, callback: (node: TreeNodeInfo) => void) {
+function forNodeAtPath(
+  nodes: TreeNodeInfo[],
+  path: NodePath,
+  callback: (node: TreeNodeInfo) => void
+) {
   callback(Tree.nodeFromPath(path, nodes));
 }
 
 export function treeReducer(state: TreeNodeInfo[], action: TreeAction) {
   switch (action.type) {
-    case "DESELECT_ALL":
+    case 'DESELECT_ALL':
       const newState1 = [...state];
-      forEachNode(newState1, node => (node.isSelected = false));
+      forEachNode(newState1, (node) => (node.isSelected = false));
       return newState1;
-    case "SET_IS_EXPANDED":
+    case 'SET_IS_EXPANDED':
       const newState2 = [...state];
-      forNodeAtPath(newState2, action.payload.path, node => (node.isExpanded = action.payload.isExpanded));
+      forNodeAtPath(
+        newState2,
+        action.payload.path,
+        (node) => (node.isExpanded = action.payload.isExpanded)
+      );
       return newState2;
-    case "SET_IS_SELECTED":
+    case 'SET_IS_SELECTED':
       const newState3 = [...state];
-      forNodeAtPath(newState3, action.payload.path, node => (node.isSelected = action.payload.isSelected));
+      forNodeAtPath(
+        newState3,
+        action.payload.path,
+        (node) => (node.isSelected = action.payload.isSelected)
+      );
       return newState3;
     default:
       return state;
